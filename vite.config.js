7d554b838bc85e3fd3a0c241e89faa50cf1291ee
Diego Melo <diego.jackchan@gmail.com>
2023-03-03 18:36:44 -0300

    bugfix: corrected base url for deploy

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     }
   },
   base: process.env.NODE_ENV === 'production'
-    ? '/front-challenge-rating/' // prod
+    ? '/front-challenge-tip/' // prod
     : '/', // dev
 })
